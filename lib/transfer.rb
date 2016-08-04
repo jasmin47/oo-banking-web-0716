8febf42fee8556f0c6a6d33a99eb57947e10ad9a
jasmin47 <alxsanborn@gmail.com>
2016-08-03 22:25:17 -0400

    all tests passing

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if @sender.balance < @amount
+      false
+    elsif @sender.valid? == true && @receiver.valid? == true
+      true
+    else false
+    end
+  end
+
+  def execute_transaction
+      if valid? == true && @status == "pending"
+      @sender.deposit(amount*(-1))
+      @receiver.deposit(amount)
+      @status = "complete"
+    else @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(amount)
+      @receiver.deposit(amount*(-1))
+      @status = "reversed"
+    end
+  end
+
 end
